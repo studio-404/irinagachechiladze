e78be440233677d5d6be6c81a0eae709be297f41
Giorgi Gvazava <hosting@404.ge>
2017-12-03 14:19:30 +0400

    pilot

---
+++ madlobt.php
@@ -0,0 +1,384 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>IRINA GACHECHILADZE'S PERSONAL PORTFOLIO</title>
+     <meta name="description" content="Georgian artist Irina Gachechiladze's Personal portfolio (Theatre arts, Directing)" />
+	<meta name="author" content="Nikoloz getsadze" />
+	<meta name="copyright" content="Irina Gachechiladze" />
+	<meta name="language" content="English" />
+	<meta nema="keywords" content="irina gachechiladze, irina gachechiladze artist,paintings,instalations,films,thatre, opera,MA in thatre arts, gachechiladze master, master gachechiladze irina,Georgian artist, irina from georgia, georgian best artists, georgian film director, irina gachechiladze thatre arts, opera arts georgia, georgia arts, tbilisi opera theatre arts, artists in tbilisi, "/>
+    
+    <link rel="stylesheet" type="text/css" href="shadow/shadowbox.css">
+<script type="text/javascript" src="shadow/shadowbox.js"></script>
+<script type="text/javascript">
+Shadowbox.init({ overlayOpacity: 0.8 }); 
+
+Shadowbox.init({
+    handleOversize: "drag",
+    modal: true
+});
+</script>
+
+
+
+<style type="text/css">
+<!--
+body {
+	font: 100%/1.4 Verdana, Arial, Helvetica, sans-serif;
+	background: #FFF;
+	margin: 0;
+	padding: 0;
+	color: #FFF;
+}
+a img { 
+	border: none;
+}
+
+a:hover, a:active, a:focus {
+	text-decoration: none;
+}
+
+.lk {
+	text-decoration: none;
+	color:#000;
+	
+}
+
+
+.lk:hover {
+	color:C5C9CE;
+	text-decoration: none;
+	
+}
+
+
+#lkin {
+	color:C5C9CE;
+}
+
+
+#niklink {
+	text-decoration: none;
+	color:#999999;
+}
+
+
+
+
+.container {
+	position:relative;
+	width:950px;
+	height:575px;
+	background: #FFF;
+	margin: 0 auto; /* the auto value on the sides, coupled with the width, centers the layout */
+	border:1px;
+	border-style: solid;
+	}
+
+
+
+
+#tag {
+	position:absolute;
+	top: 0px;
+	left:0px;
+	margin:0;
+}
+#like {
+	position:absolute;
+	top:1px;
+	width: 80px;overflow: hidden;
+	
+	left:869px;
+}
+#links {
+	position:absolute;
+	left:0px;
+	height:25px;
+	top:75px;
+	font: helvetica;
+	font-family:Helvetica,"Trebuchet MS", Arial,  sans-serif;
+	font-size:24px;
+	color:#000;
+}
+#infobox {
+	width:236px;
+	position:absolute;
+	left:0px;
+	height:416px;
+	top:135px;
+	background-color:#C5C9CE;
+	text-align:left;
+}
+
+
+#text {
+	width:220px;
+	position:absolute;
+	left:5px;
+	height:145px;
+	top:37px;
+	font:Helvetica "Trebuchet MS", Arial, , sans-serif;
+	font-size:10px;
+	line-height:13px;
+}
+
+
+#quoto{
+	position:absolute;
+	top:329px;
+	left:5px;
+	font-size:14px;
+	font-style:italic;
+}
+
+#quotoauthor{
+	position:absolute;
+	top:400px;
+	left:5px;
+	font-size:10px;
+	font: Helvetica"Trebuchet MS", Arial, , sans-serif;
+	font-style:italic;
+}
+
+
+#link1{
+	position:absolute;
+	top:5px;
+	font-size:14px;
+	left: 5px;
+	text-decoration:underline;
+}
+
+
+#link2{
+	position:absolute;
+	top:25px;
+	font-size:14px;
+	left: 5px;
+	
+}
+
+#link3{
+	position:absolute;
+	top:45px;
+	font-size:14px;
+	left: 5px;
+	
+}
+
+
+#arrow{
+	position:absolute;
+	top:2px;
+	font-size:14px;
+	left: 60px;
+	
+}
+
+#arrow2{
+	position:absolute;
+	top:2px;
+	font-size:14px;
+	left: 112px;
+	display:none;
+	
+}
+
+#arrow3{
+	position:absolute;
+	top:2px;
+	font-size:14px;
+	left: 95px;
+	display:none;
+}
+
+
+
+.linkinside{
+	font-size:14px;
+	text-decoration:underline;
+	color:FFF;
+}
+
+
+#gallery {
+	position:absolute;
+	left:235px;
+	top:135px;
+	
+}
+
+.row1{
+	margin-top:37px;
+	margin-left:2px;
+}
+.row{
+	margin-left:2px;
+}
+
+
+
+#designer {
+	position:absolute;
+	left:800px;
+	height:20px;
+	top:555px;
+	font:Helvetica "Trebuchet MS", Arial, , sans-serif;
+	font-size:10px;
+	color:#999;
+	width: 154px;
+}
+
+
+#forma{
+	position:absolute;
+	left:80px;
+	top:20px;
+	font:verdana;
+	font-size:12px;
+	color:#999;
+}
+
+#sp01{
+	position:absolute;
+	left:27px;
+	top:0px;
+	width:250px;
+	font: Helvetica"Trebuchet MS", Arial, , sans-serif;
+	text-align:left;
+	font-size:12px;
+	color:#000;
+}
+
+#sp02{
+	position:absolute;
+	left:80px;
+	top:60px;
+	font: Helvetica"Trebuchet MS", Arial, , sans-serif;
+	font-size:10px;
+	color:#000;
+}
+
+#sp03{
+	position:absolute;
+	left:80px;
+	top:110px;
+	font: Helvetica"Trebuchet MS", Arial, , sans-serif;
+	font-size:10px;
+	color:#000;
+}
+
+#sp04{
+	position:absolute;
+	left:80px;
+	top:165px;
+	font: Helvetica"Trebuchet MS", Arial, , sans-serif;
+	font-size:10px;
+	color:#000;
+}
+
+
+-->
+    </style>
+
+    </head>
+
+<?php
+
+if (isset($_POST['send'])){
+
+$data  = 'Name: '  .trim(strip_tags($_POST['name']))  ."\n";
+$data .= 'E_mail: '  .trim(strip_tags($_POST['email']))  ."\n";
+$data .= 'Comments: ' .trim(strip_tags($_POST['comment'])) ."\n";
+
+$to       = 'irinkag@hotmail.com';
+$subject  = 'www.irinagachechiladze.com';
+
+$headers  = "MIME-Version: 1.0\r\n";
+$headers .= "From: ".$_POST['email']."\n";
+$headers .= "To: ".$_POST['name']." <".$_POST['email'].">\n";
+$headers .= "Reply-To: ".$_POST['email']."\n";
+$headers .= "X-Priority: 1\n"; 
+$headers .= "X-MSMail-Priority: High\n"; 
+$headers .= "X-Mailer: My Server Mailer\n";
+$headers .= "Content-Encoding: UTF-8\r\n";
+
+if (@mail($to, $subject, $data, $headers)){
+ echo "Thank You";
+}
+
+}
+
+?>
+
+
+
+
+
+    <body>
+    <table width="100%" height="100%">
+    <td align="center">
+    
+      <div class="container">
+      <div id="like"><img src="images/like_b.jpg" width="58" height="29"></div>
+      <img src="images/name_tag.jpg" width="235" height="60" id="tag" />
+      <div id="links"><a href="index.html" target="_self" class="lk">home</a>\<a href="opera.html" target="_self" class="lk">opera</a>\<a href="theatre.html" target="_self" class="lk">theatre</a>\<a href="films.html" target="_self" class="lk" >films</a>\<a href="drawings_01.html" target="_self" class="lk">drawings</a>\<a href="instalation_01.html" target="_self" class="lk">installations</a>\<a href="contact.html" target="_self" class="lk"  id="lkin">contact</a></div>
+      
+   <div id="infobox">
+   <span id="link1"><a href="index.html" class="linkinside">Contact</a> <img src="images/arrow.jpg" width="26" height="18" id="arrow"></img></span>
+   
+ 
+   
+   
+   
+   <div id="text">
+        PO Box 6, Tbilisi, 0102, Georgia; <br>
+	Tel+995 99 33 86 10, <br>E-mail:irinka@hotmail.com; <br>
+	website: www.gachechiladze.com
+
+
+</div>
+     
+     <span id="quoto">"Here goes quoto from newspapers and other sources, lets imagine that this is s quoto from magazine" </span>
+     <span id="quotoauthor">magazine 24 hours </span>
+     
+     
+     
+     </div>
+      
+      
+      
+
+     
+     	
+		
+
+  
+  <div id="gallery"> 
+  <span id="sp01">Thank you, your message has been sent and I will be in touch as soon as I can.</span> 
+
+  
+  
+  
+  
+  </div>
+
+
+
+      
+<span id="designer"><a href="http://www.getsadze.co.uk" target="_blank" id="niklink">Designed by Niki Getsadze</a></span>
+      
+			
+      </div>
+    
+    
+    
+    </td>
+    </table>
+    
+    
+  
+</body>
+</html>
